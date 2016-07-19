193c7075cef5b77c033d1959e63d475d8a90f689
Jason Steele <steeleio1@mac.com>
2016-07-19 19:47:38 -0400

    got ajax call to work, can't get it into html

--- src/js/contactController.js
+++ src/js/contactController.js
@@ -12,8 +12,6 @@ $.ajax({
 });
 
 
-
-
 class ContactController {
 
 	constructor (form, contactsBox, addressBook) {
@@ -24,20 +22,17 @@ class ContactController {
 
 
 
-	  addContact (firstname,lastname,telephone,city,state) {
+	addContact (firstname,lastname,telephone,city,state) {
 	    // let id   = _.random(100, 999);
 	    let person = new Person(firstname,lastname,telephone,city,state);
 	    let contactHTML = this.contactTemplate(person);
-	    
-	    // add contact to address book
-	    // append contact html to the page
 	    $(".allTheContactsBox").append(contactHTML);
 	    this.addressBook.person.push(contacts);
 	  }
 
 
 
-	  formSubmit () {
+	formSubmit () {
 	    this.theForm.on('submit', (event) => {
 	      event.preventDefault();
 	      // console.log('hey')
@@ -50,6 +45,25 @@ class ContactController {
 	      this.addContact(firstname,lastname,tele,city,state);
 
 	    });
+	  }
+
+	 contactTemplate (anycontact) {
+	  	return ` 
+          <div class="contactsBox">
+            <div class= "pic2">
+              <img src="${data.picture.large}" width="100%" height=100%>
+            </div> 
+              <div class="contactsWrapper">
+                <span class="firstname2">${anycontact.first}</span>
+                <span class="lastname2">${anycontact.last}</span>
+                <span class="telephone2">${anycontact.tel}</span>
+                <span class="city2">${anycontact.city}</span>
+                <span class="state2">${anycontact.state}</span>
+                <button name="button" class="submit2">Delete</button>
+              </div>
+          </div>
+	  	`
+
 	  }
 
 
@@ -65,24 +79,7 @@ class ContactController {
   // }
 
 
-	  contactTemplate (anycontact) {
-	  	return ` 
-          <div class="contactsBox">
-            <div class= "pic2">
-              <img src="#" width="100%" height=100%>
-            </div> 
-              <div class="contactsWrapper">
-                <span class="firstname2">${anycontact.first}</span>
-                <span class="lastname2">${anycontact.last}</span>
-                <span class="telephone2">${anycontact.tel}</span>
-                <span class="city2">${anycontact.city}</span>
-                <span class="state2">${anycontact.state}</span>
-                <button name="button" class="submit2">Delete</button>
-              </div>
-          </div>
-	  	`
 
-	  }
 
 
 };
